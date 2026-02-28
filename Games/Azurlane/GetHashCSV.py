d3c2a19bd0191eb46b60dbc6f1d2a2a8f3d450a0
Wdream <2654887279@qq.com>
2026-03-01 01:24:01 +0800

    碧蓝航线 脚本更新

--- Games/Azurlane/GetHashCSV.py
+++ Games/Azurlane/GetHashCSV.py
@@ -1,7 +1,12 @@
 import socket
 from typing import Tuple, Dict
+import os
+import requests
+from datetime import datetime
 import re
 
+BASE_URL = "https://line3-patch-blhx.bilibiligame.net/android/hash/"
+OUTPUT_PATH = r"D:\Games\GameUnpackAssets\mymodel\Spine\Azurlane\LocalAssetsList"
 
 def send_tcp_request(server_ip: str, server_port: int, hex_message: str) -> bytes:
     message_bytes = bytes.fromhex(hex_message)
@@ -47,8 +52,33 @@ def get_hashfile_url() -> Tuple[str, Dict[str, str]]:
 
     return apk_urls[0], hashfile_url
 
+def download_file(url, save_dir):
+    filename = os.path.basename(url)
+    save_path = os.path.join(save_dir, filename)
+    r = requests.get(url, stream=True)
+    r.raise_for_status()
+
+    with open(save_path, "wb") as f:
+        for chunk in r.iter_content(chunk_size=8192):
+            if chunk:
+                f.write(chunk)
+
+    print("Saved to:", save_path)
 
 if __name__ == "__main__":
     apk_url, hash_urls = get_hashfile_url()
     print("APK URL:", apk_url)
     print("Hash files:", hash_urls)
+
+    az_url = BASE_URL + hash_urls['az']
+    l2d_url = BASE_URL + hash_urls['l2d']
+
+    print("AZ(Spine):", az_url)
+    print("Live2D:", l2d_url)
+
+    today_str = datetime.now().strftime("%Y-%m-%d")
+    date_dir = os.path.join(OUTPUT_PATH, today_str)
+    os.makedirs(date_dir, exist_ok=True)
+
+    download_file(az_url, date_dir)
+    download_file(l2d_url, date_dir)
